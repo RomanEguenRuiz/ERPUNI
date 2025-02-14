4f6b10a93e870cdb93c485dbcde48e424841fd99
roman <romaneguenruiz@gmail.com>
2025-02-14 14:02:22 +0100

    Initial commit

---
+++ src/main/java/com/romaneguenruiz/erpuni/ErpuniApplication.java
@@ -0,0 +1,13 @@
+package com.romaneguenruiz.erpuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romaneguenruiz/erpuni/ErpuniApplicationTests.java
@@ -0,0 +1,14 @@
+package com.romaneguenruiz.erpuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+
+}
