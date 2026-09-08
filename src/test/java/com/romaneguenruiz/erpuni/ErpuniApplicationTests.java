5ff357f6d08c060daec90be46111b96ef621b631
RomanEguenRuiz <romaneguenruiz@gmail.com>
2026-09-08 10:18:37 +0200

    initial commit

---
+++ src/test/java/com/romaneguenruiz/erpuni/ErpuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romaneguenruiz.erpuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpuniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
